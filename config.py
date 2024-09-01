585f923da43d40e823a5dd44fa954face413e9fc
sibi33 <sibiv942@gmail.com>
2024-09-01 21:37:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("6897558566:AAFBDjeIchMJh9ZIvEAuA7mKgdWH5mmxfvM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("Assassin333")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("")
+ADMIN_IDS=("2136232972")
 
 
 
